c1062a4dd8c53d6d3d7bf1c7638cfb84c70cedd6
S-Cool <silver2006@tut.by>
2016-10-24 21:51:24 +0300

    Create project WordThrough

---
+++ WordsThrough.java
@@ -0,0 +1,33 @@
+/**
+ * Created by Sergey on 25.09.2016.
+ */
+public class WordsThrough {
+
+        public static void main(String[] args)
+        {
+            String text="  Для подтверждения - электронной почты и завершения  процесса регистрации,  пройдите, пожалуйста, по ссылке  ";
+            int word=0;
+            char cur;
+            boolean space = true;
+            boolean symbol = false;
+            for (int i=0; i<text.length(); i++)
+            {
+                cur = text.charAt(i);
+                if ((cur==' ')||(space&&(cur=='-')))
+                {
+                    space=true;
+                    continue;
+                }
+                else
+                {
+                    symbol=true;
+                }
+                if (space && symbol)
+                {
+                    word++;
+                    space=false;
+                }
+            }
+            System.out.println("В тестовой строке " + word + " слов");
+        }
+    }
